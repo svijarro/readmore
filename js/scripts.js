d2a1d5b4733ba118bde5103f5550cd6ed3ee2544
svijarro <svijarro@engr-131-27.eastern.ewu.edu>
2016-05-12 14:44:22 -0700

    customize plugin

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 
 $('article').readmore({
     
-      speed: 2000,
-  moreLink: '<a href="#">Showy &#x1F600</a>',
-  lessLink: '<a href="#">Hidy &#x1F680</a>'
+    speed: 1000,
+    moreLink: '<a href="#">Showy &#x1F600</a>',
+    lessLink: '<a href="#">Hidy &#x1F680</a>'
     
 });
